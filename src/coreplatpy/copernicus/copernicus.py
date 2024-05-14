2d409a1eaa65f3989c401bcabdfc5cbd44b47722
PapanikolaouAthos <135006380+PapanikolaouAthos@users.noreply.github.com>
2024-05-14 13:23:27 +0300

    Copernicus Get Dataset and Task

--- src/coreplatpy/copernicus/copernicus.py
+++ src/coreplatpy/copernicus/copernicus.py
@@ -1,7 +1,53 @@
 import requests
 from urllib.parse import urlencode
 from ..models import ErrorReport, Folder, FolderList, PostFolder, CopyModel
+from ..models.cop_models import CopernicusTaskError, CopernicusTask, CopernicusDetails, Details
 from typing import Union
 from ..utils import safe_data_request
 
-a = numpy.array(0,0,)
+endpoint = "copernicus"
+
+def get_list(baseurl: str, service: str, token: str) -> Union[Folder, ErrorReport]:
+    uri = baseurl + endpoint + service + '/getall'
+    data = None
+    head = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+
+    response = safe_data_request('GET', uri, data, head)
+    if isinstance(response, ErrorReport):
+        return response
+    return Folder.model_validate(response)
+
+
+def get_form(baseurl: str, service: str, dataset_id: str, token: str) -> Union[Folder, ErrorReport]:
+    uri = baseurl + endpoint + '/'+ service + '/getform/' + dataset_id
+    data = None
+    head = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+
+    response = safe_data_request('GET', uri, data, head)
+    if isinstance(response, ErrorReport):
+        return response
+    return FolderList.model_validate(response)
+
+
+def get_status(baseurl: str, task_id: str, token: str) -> Union[CopernicusTask, ErrorReport]:
+
+    uri = baseurl + endpoint + '/getstatus/' + task_id
+    data = None
+    head = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+
+    response = safe_data_request('GET', uri, data, head)
+    if isinstance(response, ErrorReport):
+        return response
+    return CopernicusTask.model_validate(response)
+
+
+def post_dataset(baseurl: str, body: Details, token: str) -> Union[CopernicusDetails, ErrorReport]:
+
+    uri = baseurl + endpoint + "/dataset"
+    data = body.model_dump_json(by_alias=True)
+    head = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}
+
+    response = safe_data_request('POST', uri, data, head)
+    if isinstance(response, ErrorReport):
+        return response
+    return CopernicusDetails.model_validate(response) #do we want copernicusdetails or copericustask?
--- src/coreplatpy/models/cop_models.py
+++ src/coreplatpy/models/cop_models.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import List, Dict, Optional
+from numpy import float32
 
-class CopernicusInput(BaseModel):
-    dataset_name: str
-    body: str
 
 class Form(BaseModel):
     access_token: str
@@ -15,24 +13,98 @@ class Form(BaseModel):
     session_state: str
     scope: str
 
+
+class CopernicusTaskError(BaseModel):
+    reason: str = ""
+    message: str = ""
+    url: Optional[str] = ""
+    context: Optional[list] = []
+    permanent: Optional[bool] = False
+    who: Optional[str] = ""
+
+
+class CopernicusTask(BaseModel):
+    state: str = ""
+    location: str = ""
+    request_id: str = ""
+    message: Optional[str] = ""
+    error: Optional[CopernicusTaskError] = CopernicusTaskError()
+
+
+class CopernicusDetails(BaseModel):
+    task_id: str = ""
+    service: str = ""
+    fingerprint: str = ""
+    status: str = ""
+    error: Optional[CopernicusTaskError] = CopernicusTaskError()
+
+
+class AccordionOpts:
+    open_groups: Optional[dict]
+    searchable: Optional[bool]
+
+
+class RangeLocal:
+    e: Optional[float32]
+    n: Optional[float32]
+    w: Optional[float32]
+    s: Optional[float32]
+
+
+class CoordRange:
+    min: Optional[int]
+    max: Optional[int]
+
+
+class Coords:
+    default: Optional[int]
+    precision: Optional[int]
+    range: Optional[CoordRange]
+
+
+class Projection:
+    id: Optional[int]
+    overlay: Optional[bool]
+    use: Optional[bool]
+
+class Fields:
+    comments: Optional[str]
+    max_length: Optional[int]
+    placeholder: Optional[str]
+    required: Optional[bool]
+    type: Optional[str]
+
+
 class Details(BaseModel):
-    columns: int
-    id: int
-    labels: str
-    values: List[str]
-    accordion: bool
-    accordion_groups: bool
-    display_as_list: bool
-    full_height: bool
-    withmap: bool
-    wrapping: bool
-    precision: int
-    maximum_selections: int
-    text_file: str
-    information: str
-    accordion_options: str
-    default: str
-    extent_labels: List[str]
-    Groups: str
-    Range: str
+    columns: Optional[int]
+    id: Optional[int]
+    labels: Optional[dict]
+    values: Optional[List[str]]
+    accordion: Optional[bool]
+    accordion_groups: Optional[bool]
+    display_as_list: Optional[bool]
+    full_height: Optional[bool]
+    with_map: Optional[bool]
+    wrapping: Optional[bool]
+    precision: Optional[int]
+    maximum_selections: Optional[int]
+    text_file: Optional[str]
+    information: Optional[str]
+    accordion_options: Optional[AccordionOpts]
+    default: Optional[dict]
+    extent_labels: Optional[List[str]]
+    groups: Optional[dict]
+    range: Optional[RangeLocal]
+    change_visible: Optional[bool]
+    concat: Optional[str]
+    latitude: Optional[Coords]
+    longitude: Optional[Coords]
+    projection: Optional[Projection]
+    text: Optional[str]
+    fields: Optional[List[Fields]]
+
+
+class CopernicusInput(BaseModel):
+    dataset_name: str
+    body: Details
 
